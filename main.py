31e4a610f1f80fcb300220d3b774221e6c849d4c
simon <simon>
2020-11-22 13:59:21 +0100

    Defined db schema

--- main.py
+++ main.py
@@ -6,63 +6,81 @@ from flask_login  import current_user
 import flask_login
 from WebApp import model
 from WebApp.app import db
+from WebApp.model import SurveyState, QuestionType
 
 from datetime import datetime as dt
 
 bp = Blueprint("main", __name__)
 
-
-
 @bp.route("/")
-@flask_login.login_required
+#@flask_login.login_required
 def index():
-    messages = model.Message.query.filter_by(response_to=None).order_by(model.Message.timestamp.desc()).limit(10).all()
-    return render_template("main/index.html", posts=messages,responses=None)
+    # surveys = model.Survey.query.order_by(model.Survey.time_created.desc()).limit(10).all()
 
-@bp.route("/post/<int:message_id>")
-@bp.route('/post/<int:message_id>/<int:is_response>')
-@flask_login.login_required
-def post(message_id,is_response=0):
-    message = model.Message.query.filter_by(id=message_id).first_or_404()
-    print(message)
-    responses = model.Message.query.filter(model.Message.response_to is not None).all()#filter(model.Message.response_to==message_id).order_by(model.Message.timestamp.desc()).all()
-    responses = [response for response in responses if response.response_to is not None]
-    responses = [response for response in responses if response.response_to.id==message_id]
+    user = model.User(email="mary@example.com", name="mary")
+    surveys = [
+        model.Survey(
+            user=user,
+            survey_title="Survey Test",
+            # questions=[question1],
+            state = SurveyState.NEW,
+            time_created=datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+        model.Survey(
+            user=user,
+            survey_title="Survey Test 2",
+            # questions=[question2],
+            state = SurveyState.ONLINE,
+            time_created=datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+    ]
 
-    return render_template("main/post.html",posts=[message],with_response=bool(is_response),responses=responses)
+    return render_template("main/index.html", surveys=surveys)
 
-@bp.route("/new_post",methods=["POST"])
-@flask_login.login_required
-def create_new_post():
-    post_text = request.form.get("post_text")
-    response_to = request.form.get("response_to")
+# @bp.route("/post/<int:message_id>")
+# @bp.route('/post/<int:message_id>/<int:is_response>')
+# @flask_login.login_required
+# def post(message_id,is_response=0):
+#     message = model.Survey.query.filter_by(id=message_id).first_or_404()
+#     print(message)
+#     responses = model.Survey.query.filter(model.Survey.response_to is not None).all()#filter(model.Message.response_to==message_id).order_by(model.Message.timestamp.desc()).all()
+#     responses = [response for response in responses if response.response_to is not None]
+#     responses = [response for response in responses if response.response_to.id==message_id]
+#
+#     return render_template("main/post.html",posts=[message],with_response=bool(is_response),responses=responses)
 
-    if response_to is not None:
-        response_to = model.Message.query.filter_by(id=response_to).first_or_404()
-
-    if len(post_text)>240:
-        flash("The text was too long!")
-        return redirect(url_for("main.index"))
-    print(post_text)
-    post_timestamp = dt.now(dateutil.tz.tzlocal())
-    user = current_user
-    msg = model.Message(
-        user=user,
-        text=post_text,
-        timestamp=post_timestamp,
-        response_to=response_to
-    )
-    print(msg.user.name)
-    db.session.add(msg)
-    db.session.commit()
-    return redirect(url_for("main.post",message_id=msg.id))
+# @bp.route("/new_post",methods=["POST"])
+# @flask_login.login_required
+# def create_new_post():
+#     post_text = request.form.get("post_text")
+#     response_to = request.form.get("response_to")
+#
+#     if response_to is not None:
+#         response_to = model.Message.query.filter_by(id=response_to).first_or_404()
+#
+#     if len(post_text)>240:
+#         flash("The text was too long!")
+#         return redirect(url_for("main.index"))
+#     print(post_text)
+#     post_timestamp = dt.now(dateutil.tz.tzlocal())
+#     user = current_user
+#     msg = model.Message(
+#         user=user,
+#         text=post_text,
+#         timestamp=post_timestamp,
+#         response_to=response_to
+#     )
+#     print(msg.user.name)
+#     db.session.add(msg)
+#     db.session.commit()
+#     return redirect(url_for("main.post",message_id=msg.id))
 
 @bp.route("/profile/<int:user_id>")
-@flask_login.login_required
+#@flask_login.login_required
 def profile(user_id):
     user = model.User.query.filter_by(id=user_id).first_or_404()
-    user_messages = model.Message.query.filter_by(user=user,response_to=None).order_by(model.Message.timestamp.desc()).all()
-    return render_template("main/profile.html", user=user,posts=user_messages,responses=None)
+    user_surveys = model.Survey.query.filter_by(user=user).order_by(model.Survey.time_created.desc()).all()
+    return render_template("main/profile.html", user=user,surveys=user_messages)#,responses=None)
 
 if __name__ == "__main__":
     from . import create_app
--- model.py
+++ model.py
@@ -1,19 +1,63 @@
+#DEFINES THE SCHEMA OF OUR APPLICATION'S DATABASE
 from WebApp.app import db
 import flask_login
 from flask_login  import current_user
 
+import enum
 
 class User(flask_login.UserMixin,db.Model):
+    __table_args__ = {'extend_existing': True}
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True, nullable=False)
     name = db.Column(db.String(64), nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    messages = db.relationship('Message', backref='user', lazy=True)
+    surveys = db.relationship('Survey', backref='survey', lazy=True,cascade="all, delete-orphan",)
 
-class Message(db.Model):
+class SurveyState(enum.Enum):
+    NEW = 1
+    ONLINE = 2
+    CLOSED = 3
+
+class Survey(db.Model):
+    __table_args__ = {'extend_existing': True}
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    text = db.Column(db.String(512), nullable=False)
+    survey_title = db.Column(db.String(64), nullable=False)
+    questions = db.relationship(
+        "Question",
+        backref="survey",
+        lazy=True,
+        cascade="all, delete-orphan",
+        order_by="Question.position",
+    )
+    state = db.Column(db.Enum(SurveyState), nullable=False)
+    time_created = db.Column(db.DateTime(), nullable=False)
+    rating = db.Column(db.Float, nullable=True,default=0)
+
+class QuestionType(enum.Enum):
+    SELECT = 1
+    MULTISELECT = 2
+    TEXT = 3
+    NUMBER = 4
+    POLL = 5
+
+class Question(db.Model):
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    survey_id = db.Column(db.Integer, db.ForeignKey("survey.id"), nullable=False)
+    position = db.Column(db.Integer, autoincrement=True)
+    type = db.Column(db.Enum(QuestionType), nullable=False)
+
+class QuestionAnswer(db.Model):
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    answer_number = db.Column(db.Integer)
     timestamp = db.Column(db.DateTime(), nullable=False)
-    response_to_id = db.Column(db.Integer, db.ForeignKey('message.id'))
-    response_to = db.relationship('Message', backref='responses', remote_side=[id], lazy=True)
+
+    selected = db.Column(db.Integer)
+    text = db.Column(db.String(512))
+    number = db.Column(db.Integer)
